3628c41ba68ebbfdeb84ec4a23edff6e496f1045
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-03-03 15:05:37 +0600

    Set up project.

--- iOS-Settings/Sources/App/SettingsController.swift
+++ iOS-Settings/Sources/App/SettingsController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import SnapKit
 
 class SettingsController: UIViewController {
 
     // MARK: - Outlets
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        return tableView
+    }()
 
     // MARK: - Lifecycle
 
@@ -18,10 +25,34 @@ class SettingsController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupHierarchy()
+        setupLayout()
     }
 
     // MARK: - Setup
 
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    private func setupLayout() {
+        tableView.snp.makeConstraints {
+            $0.top.right.bottom.left.equalTo(view)
+        }
+    }
 
     // MARK: - Actions
+}
+
+// MARK: - Extensions
+
+extension SettingsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
 }
