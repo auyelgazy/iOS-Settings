19089cd3dac2b9c16d908d2e09df16d2c9404ad5
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-03-03 15:07:03 +0600

    Revert "Set up project."
    This reverts commit 9b61c62120869c02cc7d2be2e79faabbe1297389.
    
    # Conflicts:
    #	iOS-Settings/Sources/App/DetailViewController.swift
    #	iOS-Settings/Sources/App/SettingsController.swift

---
+++ iOS-Settings/Sources/App/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iOS-Settings
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 02.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
