9b61c62120869c02cc7d2be2e79faabbe1297389
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-03-03 12:49:15 +0600

    Set up project.

---
+++ iOS-Settings/Sources/App/SettingsController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOS-Settings
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 02.03.2023.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    // MARK: - Outlets
+
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Настройки"
+    }
+
+    // MARK: - Setup
+
+
+    // MARK: - Actions
+}
